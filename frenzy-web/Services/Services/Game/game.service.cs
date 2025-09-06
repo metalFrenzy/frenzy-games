c128b335e744b3d08a2f5a5241079ece348d253e
Ahmed El Sisi <Ahmed.0881100@gmail.com>
2025-09-06 23:34:07 +0300

    feat(web): add delete and update to the game services

--- frenzy-web/Services/Services/Game/game.service.cs
+++ frenzy-web/Services/Services/Game/game.service.cs
@@ -40,5 +40,43 @@ namespace FrenzyWeb.Services.Services
                 CreatedAt = game.CreatedAt
             };
         }
+
+        public async Task<bool> DeleteGame(string id)
+        {
+            var result = await _mongoDBService.Games.DeleteOneAsync(game => game.Id == id);
+            return result.DeletedCount > 0;
+        }
+
+        public async Task<GameDto?> updateGame(string id, UpdateGameDto updateGameDto)
+        {
+            var updateDefintion = Builders<Game>.Update;
+            var updates = new List<UpdateDefinition<Game>>();
+            if (!string.IsNullOrEmpty(updateGameDto.Name))
+                updates.Add(updateDefintion.Set(g => g.Name, updateGameDto.Name));
+            if (!string.IsNullOrEmpty(updateGameDto.Platform))
+                updates.Add(updateDefintion.Set(g => g.Platform, updateGameDto.Platform));
+            if (updateGameDto.Price.HasValue)
+                updates.Add(updateDefintion.Set(g => g.Price, updateGameDto.Price));
+            if (!string.IsNullOrEmpty(updateGameDto.CoverImage))
+                updates.Add(updateDefintion.Set(g => g.CoverImage, updateGameDto.CoverImage));
+            if (updates.Count == 0) return null;
+            var combinedUpdate = updateDefintion.Combine(updates);
+            var result = await _mongoDBService.Games.FindOneAndUpdateAsync(
+              g => g.Id == id,
+              combinedUpdate,
+              new FindOneAndUpdateOptions<Game> { ReturnDocument = ReturnDocument.After }
+          );
+            if (result == null) return null;
+            return new GameDto
+            {
+                Id = result.Id,
+                Name = result.Name,
+                Price = result.Price,
+                Platform = result.Platform,
+                CoverImage = result.CoverImage,
+                CreatedAt = result.CreatedAt
+            };
+
+        }
     }
 }
