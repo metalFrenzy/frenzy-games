769494fe4d19b58c8de8654015dd33e4ae1254b4
Ahmed El Sisi <Ahmed.0881100@gmail.com>
2025-09-18 21:07:39 +0300

    feat(ui): add game service functions

--- frenzy-angular/src/app/games/services/game.service.ts
+++ frenzy-angular/src/app/games/services/game.service.ts
@@ -1,9 +1,35 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { catchError, Observable } from 'rxjs';
+import { environment } from 'src/enviroments/enviroment';
+import { CreateGameDto, GameDto, UpdateGameDto } from 'src/app/models/game.models';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class GameService {
+  private baseUrl = `${environment.apiUrl}/api/games`;
+  constructor(private http: HttpClient) { }
 
-  constructor() { }
+  getGames(): Observable<GameDto[]> {
+    return this.http.get<GameDto[]>(this.baseUrl).pipe(catchError((error) => { throw error; }));
+  }
+
+  getGameById(id: string): Observable<GameDto> {
+    return this.http.get<GameDto>(`${this.baseUrl}/${id}`).pipe(catchError((error) => { throw error; }));
+  }
+
+  addGame(game: CreateGameDto): Observable<GameDto> {
+    return this.http.post<GameDto>(this.baseUrl, game).pipe(catchError((error) => { throw error; }));
+  }
+
+  deleteGame(id: string): Observable<void> {
+    return this.http.delete<void>(`${this.baseUrl}/${id}`).pipe(catchError((error) => { throw error; }));
+  }
+
+  updateGame(updatedGame: UpdateGameDto, id: string): Observable<GameDto> {
+    return this.http.put<GameDto>(`${this.baseUrl}/${id}`, updatedGame).pipe(catchError((error) => { throw error }));
+  }
+
 }
