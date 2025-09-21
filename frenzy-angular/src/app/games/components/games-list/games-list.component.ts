2e1d483f4dc092a008302d91a0a8081fcc53768f
Ahmed El Sisi <Ahmed.0881100@gmail.com>
2025-09-22 01:44:22 +0300

    feat(): fetching the game list

--- frenzy-angular/src/app/games/components/games-list/games-list.component.ts
+++ frenzy-angular/src/app/games/components/games-list/games-list.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { GameService } from '../../services/game.service';
+import { Observable } from 'rxjs';
+import { GameDto } from 'src/app/models/game.models';
 
 @Component({
   selector: 'app-games-list',
   templateUrl: './games-list.component.html',
   styleUrls: ['./games-list.component.scss']
 })
-export class GamesListComponent {
+export class GamesListComponent implements OnInit {
+  games: Observable<GameDto[]> | undefined;
+  ngOnInit(): void {
+    this.games = this.getGames();
+    console.log(this.games)
+  }
+
+  constructor(private gameService: GameService) {
+
+  }
+
+  getGames(): Observable<GameDto[]> {
+    return this.gameService.getGames();
+  }
 
 }
