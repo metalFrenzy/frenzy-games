2d126785cc6e6daab79ac2fcd3e66f824047448f
Ahmed El Sisi <Ahmed.0881100@gmail.com>
2025-10-01 23:18:49 +0300

    feat(ui): add new route

--- frenzy-angular/src/app/app-routing.module.ts
+++ frenzy-angular/src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { GamesListComponent } from './games/components/games-list/games-list.component';
 import { HomeComponent } from './games/components/home/home.component';
 import { GameDetailComponent } from './games/components/game-detail/game-detail.component';
+import { CreateGameComponent } from './games/components/create-game/create-game.component';
 
 const routes: Routes = [
   {
@@ -14,6 +15,9 @@ const routes: Routes = [
   {
     path: 'game-details/:id', component: GameDetailComponent,
   },
+  {
+    path: 'add-game', component: CreateGameComponent,
+  }
 ];
 
 @NgModule({
