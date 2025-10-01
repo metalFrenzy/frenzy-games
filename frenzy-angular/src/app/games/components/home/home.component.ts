1f669730a03dc65c4d5d3c13679a7c3ce6c634fa
Ahmed El Sisi <Ahmed.0881100@gmail.com>
2025-10-01 22:56:30 +0300

    feat(ui): link the explore games vutton to scroll to the game section

--- frenzy-angular/src/app/games/components/home/home.component.ts
+++ frenzy-angular/src/app/games/components/home/home.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from "@angular/router";
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  ngOnInit(): void {
+
+  }
+
+  constructor(private router: Router) { }
+
+
+  goToGame() {
+    const gamesSection = document.querySelector('.games-section');
+    if (gamesSection) {
+      gamesSection.scrollIntoView({ behavior: 'smooth' });
+    }
+  }
+
 
 }
